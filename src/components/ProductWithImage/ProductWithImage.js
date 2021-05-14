ce72c28886a13495e35fad4ed8b123896e5f7268
Nicola Volpi <n.volpi@aimconsulting.it>
2021-05-14 17:56:23 +0200

    Integrate product Image

--- src/components/ProductWithImage/ProductWithImage.js
+++ src/components/ProductWithImage/ProductWithImage.js
@@ -4,19 +4,29 @@ import "./productwithimage.scss";
 
 export default function ProductWithImage(props) {
   return (
-    <div className="grid__container_pruduct grid__container_pruduct_left">
-      <div className="title">
+    <div className="productWithImage__content">
+      <div className="block__black">
         <h3>{props.title}</h3>
       </div>
-      <div className="image">
-        <img src="img/wine_bottle.png" alt="" />
-      </div>
-      <div className="description">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sagittis
-        ligula quis lorem porttitor, in varius dolor hendrerit. Phasellus
-        hendrerit enim id augue porttitor, sit amet sodales magna rutrum.
-        Quisque eleifend rhoncus nisl vitae dignissim. Duis pharetra tempor
-        egestas.
+      <div
+        className="content__block"
+        style={{ backgroundColor: props.pathColor }}
+      >
+        <div
+          className="img__content"
+          style={{
+            backgroundImage: `url(${props.background})`,
+          }}
+        ></div>
+        <div
+          className="text__content"
+          dangerouslySetInnerHTML={{ __html: props.text }}
+        ></div>
+
+        <a href={props.pdf} className="hover__light">
+          Scheda tecnica
+          <img src="img/icon_download.png" alt="" />
+        </a>
       </div>
     </div>
   );
