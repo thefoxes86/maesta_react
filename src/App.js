7a49a4d910dbf870e289d593f58e2eb52b9554b4
thefoxes86 <nicola.volpi86@gmail.com>
2022-01-11 22:49:01 +0100

    Change endpoint

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
 
   useEffect(() => {
     // Mount Component
-    FetchApi("https://maestadellaformica.com/wp-json/wp/v2/posts/", {
+    FetchApi("https://backend.maestadellaformica.com/wp-json/wp/v2/posts/", {
       mode: "cors", // no-cors, *cors, same-origin
       cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
       credentials: "same-origin", // include, *same-origin, omit
