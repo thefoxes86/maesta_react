683a38e62954eb088a6fd6989107aa123463d5bf
thefoxes86 <nicola.volpi86@gmail.com>
2022-01-12 10:08:59 +0100

    Fix problem on mobile list of wine

--- src/pages/Volti/Volti.js
+++ src/pages/Volti/Volti.js
@@ -38,9 +38,8 @@ export default function Volti(props) {
         "https://backend.maestadellaformica.com/wp-json/wp/v2/media/" +
           data.featured_media,
         {
-          mode: "cors", // no-cors, *cors, same-origin
+          mode: "no-cors", // no-cors, *cors, same-origin
           cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-          credentials: "same-origin", // include, *same-origin, omit
           headers: {
             "Content-Type": "application/json",
             // 'Content-Type': 'application/x-www-form-urlencoded',
